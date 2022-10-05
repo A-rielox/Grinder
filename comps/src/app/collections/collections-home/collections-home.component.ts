9260e232a345b4e96e21f4f7d27129aff4cc8fd2
A-rielox <arielox.ag@gmail.com>
2022-10-05 15:33:35 -0500

    hasta cap 10, 1

--- comps/src/app/collections/collections-home/collections-home.component.ts
+++ comps/src/app/collections/collections-home/collections-home.component.ts
@@ -6,7 +6,22 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./collections-home.component.css'],
 })
 export class CollectionsHomeComponent implements OnInit {
+   data: { [key: string]: string | number }[] = [
+      { name: 'James', age: 24, job: 'Designer', employed: 'si' },
+      { name: 'Jill', age: 26, job: 'Engineer', employed: 'no' },
+      { age: 25, name: 'Elyse', job: 'Engineer', employed: 'si' },
+   ];
+
+   headers: { [key: string]: string }[] = [
+      { key: 'employed', label: 'Esta contratado' },
+      { key: 'name', label: 'Name' },
+      { label: 'Age', key: 'age' },
+      { key: 'job', label: 'Job' },
+   ];
+
    constructor() {}
 
    ngOnInit(): void {}
 }
+
+// { [key: string]: string; }
