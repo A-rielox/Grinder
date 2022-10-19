893aa2f3a24f006c4f10ebf94935d3f830efbe12
A-rielox <arielox.ag@gmail.com>
2022-10-18 23:57:24 -0500

    hasta cap 22 - 5

--- 21-emailclient/src/app/auth/signup/signup.component.ts
+++ 21-emailclient/src/app/auth/signup/signup.component.ts
@@ -5,8 +5,15 @@ import {
    FormGroup,
    Validators,
 } from '@angular/forms';
+import { AuthService } from '../auth.service';
 import { MatchPassword } from '../validators/match-password';
 import { UniqueUsername } from '../validators/unique-username';
+
+interface SignupCredentials {
+   username: string;
+   password: string;
+   passwordConfirmation: string;
+}
 
 @Component({
    selector: 'app-signup',
@@ -15,7 +22,7 @@ import { UniqueUsername } from '../validators/unique-username';
 })
 export class SignupComponent implements OnInit {
    // ------------- FORM-CONTROLS
-   userName = new FormControl('', {
+   username = new FormControl('', {
       validators: [
          Validators.required,
          Validators.minLength(3),
@@ -45,7 +52,7 @@ export class SignupComponent implements OnInit {
    // ------------- FORM-GROUP
    authForm = new FormGroup(
       {
-         userName: this.userName,
+         username: this.username,
          password: this.password,
          passwordConfirmation: this.passwordConfirmation,
       },
@@ -55,8 +62,37 @@ export class SignupComponent implements OnInit {
 
    constructor(
       private matchPassword: MatchPassword,
-      private uniqueUsername: UniqueUsername
+      private uniqueUsername: UniqueUsername,
+      private authService: AuthService
    ) {}
 
    ngOnInit(): void {}
+
+   onSubmit() {
+      if (this.authForm.invalid) {
+         return;
+      }
+
+      console.log(this.authForm.value);
+      //{username: 'lkjoiuyth', password: '1234', passwordConfirmation: '1234'}
+
+      // en lugar de escribir el pipe con el catchError ( p' la respuesta de nombre ocupado ) pongo el {} en el subscribe p' la accion cuando venga el error
+      // TENGO Q ESCRIBIRLAS COMO ARROW FCN PARA PODER OCUPAR " this "
+      this.authService
+         .signup(<SignupCredentials>this.authForm.value)
+         .subscribe({
+            next: (response) => {
+               // navigate to some other route
+            },
+            // complete(){},
+            error: (err) => {
+               if (!err.status) {
+                  // va a meter un error a los " errores de la form "
+                  this.authForm.setErrors({ noConnection: true });
+               } else {
+                  this.authForm.setErrors({ unknownError: true });
+               }
+            },
+         });
+   }
 }
