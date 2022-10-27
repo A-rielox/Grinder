111e36d6cf24876669d6340266d4bd0af1a776fb
A-rielox <arielox.ag@gmail.com>
2022-10-27 10:30:59 -0500

    listo cap 24 emailClient

--- 21-emailclient/src/app/inbox/email-reply/email-reply.component.ts
+++ 21-emailclient/src/app/inbox/email-reply/email-reply.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Email } from '../email';
+import { EmailService } from '../email.service';
 
 @Component({
    selector: 'app-email-reply',
@@ -6,7 +8,39 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./email-reply.component.css'],
 })
 export class EmailReplyComponent implements OnInit {
-   constructor() {}
+   @Input() email!: Email;
+
+   showModal: boolean = false;
+
+   constructor(private emailService: EmailService) {}
 
    ngOnInit(): void {}
+
+   onSubmit(e: Email) {
+      this.emailService.sendEmail(e).subscribe(() => {
+         this.showModal = false;
+      });
+   }
+
+   ngOnChanges() {
+      let { to, from, text, subject } = this.email;
+
+      text = text.replace(/\n/gi, '\n> ');
+
+      this.email = {
+         ...this.email,
+         from: to,
+         to: from,
+         subject: `RE: ${subject}`,
+         text: `\n\n\n------------ ${from} wrote:\n> ${text}`,
+      };
+
+      // MI COSECHA
+      if (!this.email.text) {
+         this.email = {
+            ...this.email,
+            text: this.email.html,
+         };
+      }
+   }
 }
